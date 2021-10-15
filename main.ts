dc7ac48ef803641bf27c87dc254896d9df3e41e6
Senskie <92569852+Senskie@users.noreply.github.com>
2021-10-15 10:48:33 +0200

    Update main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -2,26 +2,46 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     ridder_ofzo,
     [img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . f 6 6 6 6 9 6 f f . . . 
+        . . . f 6 6 6 9 9 6 f 4 5 f . . 
+        . . f 6 6 6 f 6 6 f 4 4 5 5 f f 
+        . f 6 6 6 f d f f 4 5 5 f f f . 
+        . f 6 6 6 f d 1 f 5 f f f . . . 
+        . . f 6 6 f d d f f d 2 d f f . 
+        . . . f 6 f d d f d d 2 d d f . 
+        f f f f f f f d d d d d 1 f . . 
+        f 9 9 f 9 9 f 1 1 1 1 1 f . . . 
+        f 9 6 9 6 9 f f f f f f f . . . 
+        f 9 f f f 9 f 9 9 9 f 6 f . . . 
+        f 9 6 6 6 9 f 9 9 9 f 9 f . . . 
+        f 9 6 6 6 9 f f f f f 6 f . . . 
+        . f 9 9 9 f 6 6 6 6 f f f . . . 
+        . . f f f f f f f f f f f f . . 
         `],
-    500,
+    100,
     false
     )
+})
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
+    ridder_ofzo.setImage(img`
+        . . . . . f f f f f f . . . . . 
+        . . . . f 6 6 6 6 9 6 f f . . . 
+        . . . f 6 6 6 9 9 6 f 4 5 f . . 
+        . . f 6 6 6 f 6 6 f 4 4 5 5 f f 
+        . f 6 6 6 f d f f 4 5 5 f f f . 
+        . f 6 6 6 f d 1 f 5 f f f . . . 
+        . . f 6 6 f d d f f d f d f f . 
+        . . . f 6 f d d f d d f d d f . 
+        f f f f f f f d d d d d 1 f . . 
+        f 9 9 f 9 9 f 1 1 1 1 1 f . . . 
+        f 9 6 9 6 9 f f f f f f f . . . 
+        f 9 f f f 9 f 9 9 9 f 6 f . . . 
+        f 9 6 6 6 9 f 9 9 9 f 9 f . . . 
+        f 9 6 6 6 9 f f f f f 6 f . . . 
+        . f 9 9 9 f 6 6 6 6 f f f . . . 
+        . . f f f f f f f f f f f f . . 
+        `)
 })
 let ridder_ofzo: Sprite = null
 scene.setBackgroundImage(img`
