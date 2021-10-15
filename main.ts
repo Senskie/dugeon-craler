c3f22edb73972e10542f885abafcfaf2136c8b06
Senskie <92569852+Senskie@users.noreply.github.com>
2021-10-15 14:08:46 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -206,24 +206,24 @@ scene.setBackgroundImage(img`
     dddddddddd444ddddd3ddddddddddddd4ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd44dddddddddddddddddddddddddddddddddd4ddddddddd
     `)
 ridder_ofzo = sprites.create(img`
-    . . . . . f f f f f f . . . . . 
-    . . . . f 6 6 6 6 9 6 f f . . . 
-    . . . f 6 6 6 9 9 6 f 4 5 f . . 
-    . . f 6 6 6 f 6 6 f 4 4 5 5 f f 
-    . f 6 6 6 f d f f 4 5 5 f f f . 
-    . f 6 6 6 f d 1 f 5 f f f . . . 
-    . . f 6 6 f d d f f d f d f f . 
-    . . . f 6 f d d f d d f d d f . 
-    f f f f f f f d d d d d 1 f . . 
-    f 9 9 f 9 9 f 1 1 1 1 1 f . . . 
-    f 9 6 9 6 9 f f f f f f f . . . 
-    f 9 f f f 9 f 9 9 9 f 6 f . . . 
-    f 9 6 6 6 9 f 9 9 9 f 9 f . . . 
-    f 9 6 6 6 9 f f f f f 6 f . . . 
-    . f 9 9 9 f 6 6 6 6 f f f . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f . . . . . . . 
+    . . . f f 6 6 6 6 f f . . . . . 
+    . . f 6 6 6 9 9 6 6 6 f . . . . 
+    . . f 6 6 6 6 6 9 6 6 f . . . . 
+    . f f 6 6 6 6 6 6 6 6 f f . . . 
+    . f 6 6 6 6 6 6 6 6 6 6 f . . . 
+    . f f f 6 6 6 6 6 6 f f f . . . 
+    . f 4 4 f f 6 6 f f 4 4 f . . . 
+    f f 4 5 4 4 f f 4 4 5 4 f f . . 
+    f f 4 5 5 5 4 4 5 5 5 5 f 9 f . 
+    . . f 4 5 5 5 5 5 5 4 f f 6 f . 
+    . . f f f f f f f f f f f 6 f . 
+    . f d f 6 9 6 6 9 9 f d f 6 f . 
     . . f f f f f f f f f f f f . . 
+    . . . f f f . . . f f . . . . . 
     `, SpriteKind.Player)
 ridder_ofzo.setPosition(77, 100)
 info.setLife(3)
 pause(500)
-music.playMelody("E D G F B A B C5 ", 120)
+music.playMelody("E B C5 A B G A F ", 120)
