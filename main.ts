914555e93d222b47354487ee8e9df5ebea822c39
Senskie <92569852+Senskie@users.noreply.github.com>
2021-10-15 11:04:12 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,3 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    ridder_ofzo,
-    [img`
-        . . . . . f f f f f f . . . . . 
-        . . . . f 6 6 6 6 9 6 f f . . . 
-        . . . f 6 6 6 9 9 6 f 4 5 f . . 
-        . . f 6 6 6 f 6 6 f 4 4 5 5 f f 
-        . f 6 6 6 f d f f 4 5 5 f f f . 
-        . f 6 6 6 f d 1 f 5 f f f . . . 
-        . . f 6 6 f d d f f d 2 d f f . 
-        . . . f 6 f d d f d d 2 d d f . 
-        f f f f f f f d d d d d 1 f . . 
-        f 9 9 f 9 9 f 1 1 1 1 1 f . . . 
-        f 9 6 9 6 9 f f f f f f f . . . 
-        f 9 f f f 9 f 9 9 9 f 6 f . . . 
-        f 9 6 6 6 9 f 9 9 9 f 9 f . . . 
-        f 9 6 6 6 9 f f f f f 6 f . . . 
-        . f 9 9 9 f 6 6 6 6 f f f . . . 
-        . . f f f f f f f f f f f f . . 
-        `],
-    100,
-    false
-    )
-})
 controller.A.onEvent(ControllerButtonEvent.Released, function () {
     ridder_ofzo.setImage(img`
         . . . . . f f f f f f . . . . . 
@@ -186,4 +161,46 @@ ridder_ofzo = sprites.create(img`
     `, SpriteKind.Player)
 ridder_ofzo.setPosition(77, 100)
 info.setLife(3)
-controller.moveSprite(ridder_ofzo)
+if (controller.up.isPressed()) {
+	
+} else if (controller.left.isPressed()) {
+    ridder_ofzo.setImage(img`
+        . . . . . f f f f f f . . . . . 
+        . . . f f 6 9 6 6 6 6 f . . . . 
+        . . f 5 4 f 6 9 9 6 6 6 f . . . 
+        f f 5 5 4 4 f 6 6 f 6 6 6 f . . 
+        . f f f 5 5 4 f f d f 6 6 6 f . 
+        . . . f f f 5 f 1 d f 6 6 6 f . 
+        . f f d f d f f d d f 6 6 f . . 
+        . f d d f d d f d d f 6 f . . . 
+        . . f 1 d d d d d f f f f f f f 
+        . . . f 1 1 1 1 1 f 9 9 f 9 9 f 
+        . . . f f f f f f f 9 6 9 6 9 f 
+        . . . f 6 f 9 9 9 f 9 f f f 9 f 
+        . . . f 9 f 9 9 9 f 9 6 6 6 9 f 
+        . . . f 6 f f f f f 9 6 6 6 9 f 
+        . . . f f f 6 6 6 6 f 9 9 9 f . 
+        . . f f f f f f f f f f f f . . 
+        `)
+} else if (controller.down.isPressed()) {
+	
+} else {
+    ridder_ofzo.setImage(img`
+        . . . . . f f f f f f . . . . . 
+        . . . . f 6 6 6 6 9 6 f f . . . 
+        . . . f 6 6 6 9 9 6 f 4 5 f . . 
+        . . f 6 6 6 f 6 6 f 4 4 5 5 f f 
+        . f 6 6 6 f d f f 4 5 5 f f f . 
+        . f 6 6 6 f d 1 f 5 f f f . . . 
+        . . f 6 6 f d d f f d f d f f . 
+        . . . f 6 f d d f d d f d d f . 
+        f f f f f f f d d d d d 1 f . . 
+        f 9 9 f 9 9 f 1 1 1 1 1 f . . . 
+        f 9 6 9 6 9 f f f f f f f . . . 
+        f 9 f f f 9 f 9 9 9 f 6 f . . . 
+        f 9 6 6 6 9 f 9 9 9 f 9 f . . . 
+        f 9 6 6 6 9 f f f f f 6 f . . . 
+        . f 9 9 9 f 6 6 6 6 f f f . . . 
+        . . f f f f f f f f f f f f . . 
+        `)
+}
